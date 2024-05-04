25f2128047ac228f2087ed8dd9c5f0bc35574903
Jetze Zoethout <j.zoethout22@gmail.com>
2024-05-04 22:05:37 +0200

    Ch2: Solutions

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,16 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem)
+import Data.Number (pi, pow, sqrt)
+
+diagonal ∷ Number → Number → Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea ∷ Number → Number
+circleArea r = pi * pow r 2.0
+
+leftoverCents :: Int -> Int
+leftoverCents cents = cents `rem` 100
+-- But of course, mod is always better :)
